c9607e8b689176c3cfad379374b90ff36d731450
Surafel Takele <118208119+Sura-T@users.noreply.github.com>
2024-10-18 09:18:20 +0300

    Time: 0 ms (100.00%) | Memory: 11.5 MB (88.70%) - LeetSync

---
+++ 66-plus-one/plus-one.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        return [int(digit) for digit in str(int(''.join(map(str, digits))) + 1)]
