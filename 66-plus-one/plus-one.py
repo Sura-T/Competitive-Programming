6b30b6b2098116410a457af2d3d0487349eccce8
Surafel Takele <118208119+Sura-T@users.noreply.github.com>
2025-02-02 21:58:34 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (7.11%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,7 +1,6 @@
-class Solution(object):
-    def plusOne(self, digits):
-        """
-        :type digits: List[int]
-        :rtype: List[int]
-        """
-        return [int(digit) for digit in str(int(''.join(map(str, digits))) + 1)]
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        digits = [str(char) for char in digits]
+        str_digits = "".join(digits)
+        digits = str(int(str_digits) + 1)
+        return [int(digit) for digit in digits]
