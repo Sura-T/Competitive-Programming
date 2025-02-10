c859c8c003743d2eba54baf5232471e4fe524457
Surafel Takele <118208119+Sura-T@users.noreply.github.com>
2025-02-10 11:18:16 +0300

    Time: 247 ms (9.50%) | Memory: 18.2 MB (65.23%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,16 +1,13 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        my_dict = {}
-        res = []
-        result = []
-        for i in range(len(names)):
-            my_dict[heights[i]] = names[i]
-            
-        for key in my_dict:
-            res.append(key)
-        res.sort(reverse=True)
-
-        for i in res:
-            result.append(my_dict[i])
-
-        return result
+        res = [(heights[i], names[i]) for i in range(len(names))]
+        
+        for i in range(1, len(res)):
+            height = res[i]
+            j = i - 1
+            while j >= 0 and res[j][0] < height[0]: 
+                res[j + 1] = res[j]
+                j -= 1
+            res[j + 1] = height
+        
+        return [name[1] for name in res]
