bec0e8a0d028bec12c354abf5fd0e0e1534e2483
Surafel Takele <118208119+Sura-T@users.noreply.github.com>
2025-02-10 11:48:00 +0300

    Time: 207 ms (11.80%) | Memory: 18.3 MB (24.31%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,13 +1,16 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        res = [(heights[i], names[i]) for i in range(len(names))]
+        #counting sorting method
+        height = defaultdict(list)
         
-        for i in range(1, len(res)):
-            height = res[i]
-            j = i - 1
-            while j >= 0 and res[j][0] < height[0]: 
-                res[j + 1] = res[j]
-                j -= 1
-            res[j + 1] = height
+        max_height = max(heights)
         
-        return [name[1] for name in res]
+        for i in range(len(names)):
+            height[heights[i]].append(names[i])
+        
+        result = []
+        for h in range(max_height, -1, -1):  
+            if h in height:
+                result.extend(height[h])  
+        
+        return result
